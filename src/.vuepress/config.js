c73959a533b442d583ae29034d882d3bf9b254a2
Forest <767425412lin@gmail.com>
2022-04-13 00:10:16 +0800

    feat: 集成eslint、prettier、editorconfig

---
+++ src/.vuepress/config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  // 站点配置
+  lang: 'zh-CN',
+  title: 'toimc admin docs',
+  description: 'toimc admin docs',
+
+  // 主题和它的配置
+  theme: '@vuepress/theme-default',
+  themeConfig: {
+    logo: 'https://vuejs.org/images/logo.png'
+  }
+}
